84678ffaa3a431913449c35703a8b2e71db78124
darrench3140 <darrench3140@gmail.com>
2023-02-26 01:43:56 +0800

    dev

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
   const fetchPosts = async () => {
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:8080/api/v1/post", {
+      const response = await fetch("https://dall-e-vzmz.onrender.com/api/v1/post", {
         method: "GET",
         headers: { "Content-Type": "application/json" },
       });
@@ -51,7 +51,7 @@ const Home = () => {
       setTimeout(() => {
         const searchResults = allPosts.filter(
           (item) =>
-            item.name.toLowerCase().include(searchText.toLowerCase()) ||
+            item.name.toLowerCase().includes(searchText.toLowerCase()) ||
             item.prompt.toLowerCase().includes(searchText.toLowerCase())
         );
         setSearchedResults(searchResults);
@@ -102,6 +102,7 @@ const Home = () => {
               ) : (
                 <RenderCards data={allPosts} title="No Posts Yet" />
               )}
+              { console.log("what the fuck" + allPosts)}
             </div>
           </>
         )}
